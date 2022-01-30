f1eb308bc81a9d4e85e10eb39dc55c16f3a734b7
Boonyarit Eoyeye <yim.boonyarit@gmail.com>
2022-01-30 18:02:41 +0700

    update travis and deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker build -t boonyaritth7/muti-client:latest -t boonyaritth7/muti-client:$SHA -f ./client/DOckerfile ./client
+docker build -t boonyaritth7/muti-server:latest -t boonyaritth7/muti-server:$SHA -f ./server/DOckerfile ./server
+docker build -t boonyaritth7/muti-worker:latest -t boonyaritth7/muti-worker:$SHA -f ./worker/DOckerfile ./worker
+
+docker push boonyaritth7/muti-client:latest
+docker push boonyaritth7/muti-server:latest
+docker push boonyaritth7/muti-worker:latest
+
+docker push boonyaritth7/muti-client:$SHA
+docker push boonyaritth7/muti-server:$SHA
+docker push boonyaritth7/muti-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=boonyaritth7/muti-server:$SHA
+kubectl set image deployments/client-deployment client=boonyaritth7/muti-client:$SHA
+kubectl set image deployments/worker-deployment worker=boonyaritth7/muti-worker:$SHA
+
